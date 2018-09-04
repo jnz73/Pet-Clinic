f992939ee147444d0c82a7852ae907396037d356
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-09-04 18:10:30 +0200

    adding POJOS. Closes #2.

---
+++ src/main/java/com/gianni/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.gianni.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/gianni/petclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package com.gianni.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType type;
+    private Owner owner;
+    private LocalDate birthdate;
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
