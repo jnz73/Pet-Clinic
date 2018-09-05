593b535bb008a83ecb343c685778d934a720c30e
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-09-05 14:09:47 +0200

    refactor services to common interface. closes #11

--- pet-clinic-model/src/main/java/com/gianni/petclinic/services/PetService.java
+++ pet-clinic-model/src/main/java/com/gianni/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.gianni.petclinic.services;
 
 import com.gianni.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
