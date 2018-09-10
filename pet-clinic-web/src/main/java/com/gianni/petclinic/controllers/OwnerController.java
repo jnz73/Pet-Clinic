3b6a8928ff97269ff1dac30d1e94e7b5aba81dd6
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-09-10 14:18:55 +0200

    List all Owners on index page . closes #17

--- pet-clinic-web/src/main/java/com/gianni/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/gianni/petclinic/controllers/OwnerController.java
@@ -1,12 +1,22 @@
 package com.gianni.petclinic.controllers;
 
+import com.gianni.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
-    @RequestMapping({"/owners","/owners/index", "owners/index.html"})
-    public String listOwners(){
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"/owners", "/owners/index", "owners/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "/owners/index";
     }
 }
