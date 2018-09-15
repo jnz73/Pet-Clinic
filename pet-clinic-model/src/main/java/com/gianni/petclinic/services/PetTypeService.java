b20638b98e3545696afbd0efb5d0b4055a35693b
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-09-15 17:27:16 +0200

    add pet type map service. closes #30

---
+++ pet-clinic-model/src/main/java/com/gianni/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.gianni.petclinic.services;
+
+import com.gianni.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-model/src/main/java/com/gianni/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.gianni.petclinic.services.map;
+
+import com.gianni.petclinic.model.PetType;
+import com.gianni.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
