d6c9dcfef2af62c524cc660fb90670b1d534a0ad
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-09-17 13:38:05 +0200

    Implement Speciality Map Service. closes #32

---
+++ pet-clinic-model/src/main/java/com/gianni/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.gianni.petclinic.services;
+
+import com.gianni.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-model/src/main/java/com/gianni/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package com.gianni.petclinic.services.map;
+
+import com.gianni.petclinic.model.Speciality;
+import com.gianni.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
