49a2083f185fb22518aa645ee4fb5fdc83957494
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-09-21 15:18:14 +0200

    Create Spring Data JPA Service for Vets . closes #42

--- pet-clinic-model/src/main/java/com/gianni/petclinic/repositories/VetRepository.java
+++ pet-clinic-model/src/main/java/com/gianni/petclinic/repositories/VetRepository.java
@@ -3,5 +3,5 @@ package com.gianni.petclinic.repositories;
 import com.gianni.petclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
-public interface VetLRepository extends CrudRepository<Vet, Long> {
+public interface VetRepository extends CrudRepository<Vet, Long> {
 }
